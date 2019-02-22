41c007a217630b9da7e4a7f28ebf213eb081ec50
navaato305 <navaato305@hotmail.com>
2019-02-22 00:48:20 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# online-car-game
+#Multiplayer Node Car Game 
+
+Game built with Node, Socket.io, Babel, and Phaser.
+
+https://mighty-mountain-36812.herokuapp.com/
---
+++ client/state/world/createWorld.js
@@ -0,0 +1,22 @@
+import { WORLD_SIZE } from './../../config'
+
+const { width, height } = WORLD_SIZE
+
+const worldCreator = game => {
+  game.physics.startSystem(Phaser.Physics.P2JS)
+  game.stage.disableVisibilityChange = true
+  game.world.setBounds(0, 0, width, height)
+  createMap(game)
+}
+
+const createMap = game => {
+  let groundTiles = []
+  for (let i = 0; i <= width / 64 + 1; i++) {
+    for (let j = 0; j <= height / 64 + 1; j++) {
+      const groundSprite = game.add.sprite(i * 64, j * 64, 'asphalt')
+      groundTiles.push(groundSprite)
+    }
+  }
+}
+
+export default worldCreator
